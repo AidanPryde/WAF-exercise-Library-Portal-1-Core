3a04deccd263baf50f5a82f3ce26a7c9fa953245
Peskó <Marton.Pesko@t-systems.com>
2018-04-26 11:10:38 +0200

    Add project files.

---
+++ WAF-exercise-Library-Portal-1-Core-WA/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WAF_exercise_Library_Portal_1_Core_WA.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WAF-exercise-Library-Portal-1-Core-WA/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WAF_exercise_Library_Portal_1_Core_WA
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
